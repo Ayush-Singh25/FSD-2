62576e4e51b2d0dab61a8479fe27d6f38ab6749f
Ayush <ayush.kl2530@gmail.com>
2026-01-16 11:18:32 +0530

    exp2 added

--- expirement-1/src/App.jsx
+++ expirement-1/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import SinglePageApp from './components/spa'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       Ayush Singh
+      <SinglePageApp/> 
     </>
   )
 }
 
 export default App
+
